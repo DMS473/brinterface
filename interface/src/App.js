5c36842eaa50653d3d33bb6ee3e2ad3e621dffee
DMS473 <daffamuis0407@gmail.com>
2024-08-28 20:41:00 +0700

    first push

---
+++ interface/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App Container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
